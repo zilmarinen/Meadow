5f2a96b4b0da4a870f73465869f10a92ad85e5b7
Zack Brown <zack@zackbrown.co.uk>
2020-12-05 13:33:47 +0000

    Fix incorrect footpath tile type enum case

--- Sources/Meadow/Footpath/FootpathTileType.swift
+++ Sources/Meadow/Footpath/FootpathTileType.swift
@@ -33,7 +33,7 @@ public enum FootpathTileType: Int, CaseIterable, Codable, Equatable {
         case .dirt: return Color(red: 0.92, green: 0.78, blue: 0.53, alpha: 1.0)
         case .gravel: return Color(red: 0.8, green: 0.78, blue: 0.53, alpha: 1.0)
         case .stone: return Color(red: 0.95, green: 0.57, blue: 0.2, alpha: 1.0)
-        case .unknown: return Color(red: 0.8, green: 0.89, blue: 0.45, alpha: 1.0)
+        case .cobble: return Color(red: 0.8, green: 0.89, blue: 0.45, alpha: 1.0)
         case .wood: return Color(red: 0.54, green: 0.8, blue: 0.8, alpha: 1.0)
         }
     }

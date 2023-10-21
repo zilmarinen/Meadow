950562c9d0f40ae18bda79ec2585843db3336a0c
Zack Brown <zack@zackbrown.co.uk>
2023-10-21 15:56:41 +0100

    Update gitignore and package description

--- Sources/Meadow/Meadow.swift
+++
@@ -1,6 +0,0 @@
-public struct Meadow {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
---
+++ Sources/Meadow/Terrain/Terrain.swift
@@ -0,0 +1,12 @@
+//
+//  Terrain.swift
+//
+//  Created by Zack Brown on 02/09/2023.
+//
+
+import Bivouac
+import SceneKit
+
+public final class Terrain: SCNNode {
+    
+}
